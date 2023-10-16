d006a1fc2462b7ce1cdf0ce7cfd2ca3cbc54e61d
David_Andrade <david.andrade01@live.com>
2023-10-16 13:47:07 +0100

    add server url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const ENV={
 
-    SERVER_HOST:"http://localhost:1337",
-    API_URL:"http://localhost:1337/api",
+    SERVER_HOST:"https://gamestoreserver-production-9328.up.railway.app",
+    API_URL:"https://gamestoreserver-production-9328.up.railway.app/api",
     ENDPOINTS: {
         AUTH: {
           REGISTER: "auth/local/register",
